503d405e00a096beb39aa93e4d424f357fc14422
Richard Wylie <richardwylie32@googlemail.com>
2025-08-30 12:45:12 +0100

    latest

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "25a8af974384c8d55a5014a1b14675c1",
+const RESOURCES = {"flutter_bootstrap.js": "266e92b15cac544a7e2536d5f581e7a1",
 "version.json": "f449fa1184840b24dabe24823ef32240",
-"index.html": "6ad49be8399f3afc44ba8267ddfcc9b4",
-"/": "6ad49be8399f3afc44ba8267ddfcc9b4",
+"index.html": "fbe8a0929d547a97a6acf185d6a09c65",
+"/": "fbe8a0929d547a97a6acf185d6a09c65",
 "main.dart.js": "45bf05756a89d6d225d30bd5de66bfeb",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
@@ -14,7 +14,7 @@ const RESOURCES = {"flutter_bootstrap.js": "25a8af974384c8d55a5014a1b14675c1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"manifest.json": "980c959969ef340832e0b56fe46facb7",
+"manifest.json": "5f4e44d1afbb7b3b83230a8e7e656710",
 "assets/AssetManifest.json": "8a4a1c0318930bf453d406c56d1c5f9c",
 "assets/NOTICES": "910c2689e2127076c6e443b7cf15095e",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
